4b97f2ddadf50f0c4e8e2956e6b6bd08d1ae699b
Omega <83683090+Omega172@users.noreply.github.com>
2024-02-22 17:01:03 -0600

    Changed renderer again

--- OmegaWare Framework/Hooks/Renderer/RendererHooks.h
+++ OmegaWare Framework/Hooks/Renderer/RendererHooks.h
@@ -1,14 +1,6 @@
 #pragma once
 #include "pch.h"
 
-#if FRAMEWORK_RENDER_D3D11
-#include "Hooks/Renderer/D3D11/D3D11Hooks.h"
-#endif
-
-#if FRAMEWORK_RENDER_D3D12
-#include "Hooks/Renderer/D3D12/D3D12Hooks.h"
-#endif
-
 class RendererHooks
 {
 private:
@@ -16,22 +8,6 @@ private:
 
 public:
 
-	bool Setup()
-	{
-#if FRAMEWORK_RENDER_D3D11
-		Initialized = D3D11Setup();
-		return Initialized;
-#endif
-
-		return false;
-	}
-
-	void Destroy()
-	{
-#if FRAMEWORK_RENDER_D3D11
-		D3D11Destroy();
-#endif
-
-		return;
-	}
+	bool Setup();
+	void Destroy();
 };
