d06255373fe8e4d61d962d841d5801c1bf5abf3c
Good Evening <114830237+GoodEveningFellOff@users.noreply.github.com>
2024-11-25 00:31:13 -0500

    COLORS SO MANY COLORS

--- Internal/Utils/Logging/Colors.hpp
+++ Internal/Utils/Logging/Colors.hpp
@@ -1,407 +1,47 @@
-#ifndef COLORS_HPP_
-#define COLORS_HPP_
-
-#include <iostream>
-
-#if defined(_WIN32) || defined(_WIN64)
-#   define COLORS_TARGET_WINDOWS
-#elif defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))
-#   define COLORS_TARGET_POSIX
-#endif
-
-#if !defined(COLORS_USE_ANSI_ESCAPE) && !defined(COLORS_USE_WINDOWS_API)
-#   if defined(COLORS_TARGET_POSIX)
-#       define COLORS_USE_ANSI_ESCAPE
-#   elif defined(COLORS_TARGET_WINDOWS)
-#       define COLORS_USE_WINDOWS_API
-#   endif
-#endif
-
-#if defined(COLORS_USE_WINDOWS_API)
-#   include <Windows.h>
-#endif
+#pragma once
 
 // Who the fuck defines stream functions for this stuff???
 namespace colors {
-    inline constexpr auto reset     = "\e[00m";
-    inline constexpr auto bold      = "\e[1m";
-    inline constexpr auto faint     = "\e[2m";
-    inline constexpr auto italic    = "\e[3m";
-    inline constexpr auto underline = "\e[4m";
-    inline constexpr auto blink     = "\e[5m";
-
-    inline std::ostream& italic(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[3m";
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& underline(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[4m";
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& blink(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[5m";
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& reverse(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[7m";
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& invisible(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[8m";
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& strikethrough(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[9m";
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& grey(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[30m";
-#else
-        wset_attributes(0);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& red(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[31m";
-#else
-        wset_attributes(FOREGROUND_RED);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& green(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[32m";
-#else
-        wset_attributes(FOREGROUND_GREEN);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& yellow(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[33m";
-#else
-        wset_attributes(FOREGROUND_RED | FOREGROUND_GREEN);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& blue(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[34m";
-#else
-        wset_attributes(FOREGROUND_BLUE);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& magenta(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[35m";
-#else
-        wset_attributes(FOREGROUND_RED | FOREGROUND_BLUE);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& cyan(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[36m";
-#else
-        wset_attributes(FOREGROUND_BLUE | FOREGROUND_GREEN);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& white(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[37m";
-#else
-        wset_attributes(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& bright_grey(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[90m";
-#else
-        wset_attributes(0 | FOREGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& bright_red(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[91m";
-#else
-        wset_attributes(FOREGROUND_RED | FOREGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& bright_green(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[92m";
-#else
-        wset_attributes(FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& bright_yellow(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[93m";
-#else
-        wset_attributes(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& bright_blue(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[94m";
-#else
-        wset_attributes(FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& bright_magenta(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[95m";
-#else
-        wset_attributes(FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& bright_cyan(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[96m";
-#else
-        wset_attributes(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& bright_white(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[97m";
-#else
-        wset_attributes(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_grey(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[40m";
-#else
-        wset_attributes(-1, 0);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_red(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[41m";
-#else
-        wset_attributes(-1, BACKGROUND_RED);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_green(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[42m";
-#else
-        wset_attributes(-1, BACKGROUND_GREEN);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_yellow(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[43m";
-#else
-        wset_attributes(-1, BACKGROUND_RED | BACKGROUND_GREEN);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_blue(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[44m";
-#else
-        wset_attributes(-1, BACKGROUND_BLUE);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_magenta(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[45m";
-#else
-        wset_attributes(-1, BACKGROUND_RED | BACKGROUND_BLUE);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_cyan(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[46m";
-#else
-        wset_attributes(-1, BACKGROUND_BLUE | BACKGROUND_GREEN);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_white(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[47m";
-#else
-        wset_attributes(-1, BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_bright_grey(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[100m";
-#else
-        wset_attributes(-1, 0 | BACKGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_bright_red(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[101m";
-#else
-        wset_attributes(-1, BACKGROUND_RED | BACKGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_bright_green(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[102m";
-#else
-        wset_attributes(-1, BACKGROUND_GREEN | BACKGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_bright_yellow(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[103m";
-#else
-        wset_attributes(-1, BACKGROUND_GREEN | BACKGROUND_RED | BACKGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_bright_blue(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[104m";
-#else
-        wset_attributes(-1, BACKGROUND_BLUE | BACKGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_bright_magenta(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[105m";
-#else
-        wset_attributes(-1, BACKGROUND_BLUE | BACKGROUND_RED | BACKGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_bright_cyan(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[106m";
-#else
-        wset_attributes(-1, BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-
-    inline std::ostream& on_bright_white(std::ostream& stream) {
-#if defined(COLORS_USE_ANSI_ESCAPE)
-        stream << "\033[107m";
-#else
-        wset_attributes(-1, BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED | BACKGROUND_INTENSITY);
-#endif
-
-        return stream;
-    }
-}
-
-#undef COLORS_TARGET_POSIX
-#undef COLORS_TARGET_WINDOWS
-#undef COLORS_USE_ANSI_ESCAPE
-#undef COLORS_USE_WINDOWS_API
-
-#endif
+    inline constexpr auto reset         = "\e[00m";
+    inline constexpr auto bold          = "\e[1m";
+    inline constexpr auto faint         = "\e[2m";
+    inline constexpr auto italic        = "\e[3m";
+    inline constexpr auto underline     = "\e[4m";
+    inline constexpr auto blink         = "\e[5m";
+    inline constexpr auto invisible     = "\e[6m";
+    inline constexpr auto strikethrough = "\e[7m";
+    // Foreground color.
+    inline constexpr auto grey           = "\e[30m";
+    inline constexpr auto red            = "\e[31m";
+    inline constexpr auto green          = "\e[32m";
+    inline constexpr auto yellow         = "\e[33m";
+    inline constexpr auto blue           = "\e[34m";
+    inline constexpr auto magenta        = "\e[35m";
+    inline constexpr auto cyan           = "\e[36m";
+    inline constexpr auto white          = "\e[37m";
+    inline constexpr auto bright_grey    = "\e[90m";
+    inline constexpr auto bright_red     = "\e[91m";
+    inline constexpr auto bright_green   = "\e[92m";
+    inline constexpr auto bright_yellow  = "\e[93m";
+    inline constexpr auto bright_blue    = "\e[94m";
+    inline constexpr auto bright_magenta = "\e[95m";
+    inline constexpr auto bright_cyan    = "\e[96m";
+    inline constexpr auto bright_white   = "\e[97m";
+    // Background color
+    inline constexpr auto on_grey           = "\e[40m";
+    inline constexpr auto on_red            = "\e[41m";
+    inline constexpr auto on_green          = "\e[42m";
+    inline constexpr auto on_yellow         = "\e[43m";
+    inline constexpr auto on_blue           = "\e[44m";
+    inline constexpr auto on_magenta        = "\e[45m";
+    inline constexpr auto on_cyan           = "\e[46m";
+    inline constexpr auto on_white          = "\e[47m";
+    inline constexpr auto on_bright_grey    = "\e[100m";
+    inline constexpr auto on_bright_red     = "\e[101m";
+    inline constexpr auto on_bright_green   = "\e[102m";
+    inline constexpr auto on_bright_yellow  = "\e[103m";
+    inline constexpr auto on_bright_blue    = "\e[104m";
+    inline constexpr auto on_bright_magenta = "\e[105m";
+    inline constexpr auto on_bright_cyan    = "\e[106m";
+    inline constexpr auto on_bright_white   = "\e[107m";
+};
