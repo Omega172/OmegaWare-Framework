4bd9f936295980beda8cadc7b240ebd7387cece8
Omega <83683090+Omega172@users.noreply.github.com>
2024-12-26 04:30:34 -0600

    Update UnrealConfig.h

--- Internal/Interfaces/Unreal/UnrealConfig.h
+++ Internal/Interfaces/Unreal/UnrealConfig.h
@@ -3,8 +3,10 @@
 
 #include "SDK.h"
 
+#define DUMPER_7 0
+
 #define SDK_IMPORTED 0
-static_assert(SDK_IMPORTED, "Did you remember to copy in the CG SDK?");
+static_assert(SDK_IMPORTED, "Did you remember to copy in the SDK?");
 
 // Reminder remember to incude these files in the project
 // BasicTypes_Package.cpp
